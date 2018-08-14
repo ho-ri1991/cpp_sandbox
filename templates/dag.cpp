ede27f2ec73dfa9d87353485ce0abb9bc495e9f9
ho-ri1991 <hage.1991@hotmail.co.jp>
2018-08-14 23:48:07 +0900

    create static dag project

---
+++ templates/dag.cpp
@@ -0,0 +1,268 @@
+#include <type_traits>
+#include <tuple>
+
+namespace my
+{
+  template <typename T>
+  struct identity
+  {
+    using type = T;
+  };
+
+  template <template <typename...> class Class, typename T>
+  struct is_match_template: std::false_type{};
+
+  template <template <typename...> class Class, typename ...Args>
+  struct is_match_template<Class, Class<Args...>>: std::true_type{};
+
+  template <template <typename...> class Class, typename T>
+  constexpr bool is_match_template_v = is_match_template<Class, T>::value;
+
+  template <typename... Ts>
+  struct type_list {};
+  
+  namespace type_list_meta
+  {
+    template <typename TypeList>
+    struct head;
+    template <typename Head, typename... Tail>
+    struct head<type_list<Head, Tail...>>
+    {
+      using type = Head;
+    };
+    template <typename...Ts>
+    using head_t = typename head<Ts...>::type;
+    template <typename TypeList>
+    struct tail;
+    template <typename Head, typename... Tail>
+    struct tail<type_list<Head, Tail...>>
+    {
+      using type = type_list<Tail...>;
+    };
+    template <typename... Ts>
+    using tail_t = typename tail<Ts...>::type;
+    template <typename TypeList, typename...TypeLists>
+    struct merge
+    {
+      using type = typename merge<TypeList, typename merge<TypeLists...>::type>::type;
+    };
+    template <typename...Ts1, typename... Ts2>
+    struct merge<type_list<Ts1...>, type_list<Ts2...>>
+    {
+      using type = type_list<Ts1..., Ts2...>;
+    };
+    template <typename... Ts>
+    struct merge<type_list<Ts...>>
+    {
+      using type = type_list<Ts...>;
+    };
+    template <typename... TypeLists>
+    using merge_t = typename merge<TypeLists...>::type;
+    template <typename TypeList>
+    struct reverse
+    {
+      using type = merge_t<typename reverse<tail_t<TypeList>>::type, type_list<head_t<TypeList>>>;
+    };
+    template <>
+    struct reverse<type_list<>>
+    {
+      using type = type_list<>;
+    };
+    template <typename TypeList>
+    using reverse_t = typename reverse<TypeList>::type;
+    template <typename TypeList>
+    struct last
+    {
+      using type = head_t<reverse_t<TypeList>>;
+    };
+    template <>
+    struct last<type_list<>> {};
+    template <typename TypeList>
+    using last_t = typename last<TypeList>::type;
+  }
+
+  struct terminal_node {};
+  static constexpr auto teminal = terminal_node{};
+  template <typename T>
+  struct init_next
+  {
+    static constexpr T* value = nullptr;
+  };
+  template <>
+  struct init_next<terminal_node>
+  {
+    static constexpr terminal_node terminal{};
+    static constexpr const terminal_node* value = &terminal;
+  };
+
+  template <typename Next = terminal_node>
+  struct inner_node
+  {
+    template <typename T>
+    using this_template = inner_node<T>;
+    template <typename T>
+    this_template<T> copy() const { return this_template<T>{}; }
+//    Next* next = init_next<Next>();
+  };
+
+  template <typename N1, typename N2, typename... Ns>
+  struct chained_node
+  {
+    using head_type = N1;
+    using last_type = typename std::conditional_t<sizeof...(Ns) == 0, identity<N2>, type_list_meta::last<type_list<Ns...>>>::type;
+    static constexpr auto chain_size = sizeof...(Ns) + 2;
+    std::tuple<N1, N2, Ns...> ns;
+  };
+
+  template <typename N1, typename N2, typename... Ns>
+  struct or_node
+  {
+    using content_types = type_list<N1, N2, Ns...>;
+    std::tuple<N1, N2, Ns...> ns;
+    auto get_head_pointers();
+  };
+
+  template <typename Node>
+  struct node_heads
+  {
+    using type = type_list<Node>;
+    static auto get_head_pointers(Node& node) { return std::make_tuple(&node); }
+  };
+  template <typename N1, typename... Ns>
+  struct node_heads<chained_node<N1, Ns...>>
+  {
+    using type = typename node_heads<N1>::type;
+    static auto get_head_pointers(chained_node<N1, Ns...>& node) { return node_heads<N1>::get_head_pointers(std::get<0>(node.ns)); }
+  };
+  template <typename N1, typename... Ns>
+  struct node_heads<or_node<N1, Ns...>>
+  {
+    using type = type_list_meta::merge_t<typename node_heads<N1>::type, typename node_heads<Ns>::type...>;
+    template <std::size_t... Is>
+    static auto get_head_pointers_impl(or_node<N1, Ns...>& node, std::index_sequence<Is...>)
+    {
+      return std::tuple_cat(node_heads<Ns>::get_head_pointers(std::get<Is + 1>(node.ns))...);
+    }
+    static auto get_head_pointers(or_node<N1, Ns...>& node)
+    {
+      return std::tuple_cat(
+        node_heads<N1>::get_head_pointers(std::get<0>(node.ns)),
+        get_head_pointers_impl(node, std::make_index_sequence<sizeof...(Ns)>{})
+      );
+    }
+  };
+
+
+  template <typename Tuple, std::size_t... Is>
+  auto tail_impl(Tuple&& t, std::index_sequence<Is...>)
+  {
+    return std::make_tuple(std::get<Is + 1>(std::forward<Tuple>(t))...);
+  }
+
+  template <typename Tuple>
+  auto tail(Tuple&& t)
+  {
+    return tail_impl(std::forward<Tuple>(t), std::make_index_sequence<std::tuple_size_v<Tuple> - 1>{});
+  }
+
+  template <typename T, typename... Ts>
+  auto remove_last(const std::tuple<T, Ts...>& t)
+  {
+    if constexpr (sizeof...(Ts) == 0)
+      return std::tuple<>{};
+    else
+      return std::tuple_cat(std::tuple<T>{std::get<0>(t)}, remove_last(tail(t)));
+  }
+
+//  template <typename N1, typename N2>
+//  auto operator+(N1&& n1, N2&& n2)
+//  {
+//    using Node1 = std::decay_t<N1>;
+//    using Node2 = std::decay_t<N2>;
+//    if constexpr (
+//      is_match_template_v<chained_node, Node1> &&
+//      is_match_template_v<chained_node, Node2>
+//    )
+//    { // both nodes are chained node
+//      if constexpr (
+//        is_match_template_v<or_node, typename Node1::last_type> &&
+//        is_match_template_v<or_node, typename Node2::head_type>
+//      )
+//      { // joint points are both or_node
+//      }
+//      else if constexpr (
+//        is_match_template_v<or_node, typename Node1::las_typet> &&
+//        !is_match_template_v<or_node, typename Node2::head_type>
+//      )
+//      { // joint point of Node1 is or_node, that of Node2 is not
+//      }
+//      else if constexpr (
+//        !is_match_template_v<or_node, typename Node1::last_type> &&
+//        is_match_template_v<or_node, typename Node2::head_type>
+//      )
+//      { // joint point of Node2 is or_node, that of Node1 is not
+//      }
+//      else
+//      {
+//        auto ans = std::tuple_cat(
+//          remove_last(n1),
+//          std::make_tuple(std::get<Node1::chain_size - 1>(n1).copy<typename Node2::head_type>()),
+//          n2
+//        );
+//        // update pointer
+//      }
+//    }
+//    else if constexpr (
+//  }
+//  template <typename N1, typename N2>
+//  auto operator|(N1&& n1, N2&& n2)
+//  {
+//    using Node1 = std::decay_t<N1>;
+//    using Node2 = std::decay_t<N2>;
+//  }
+}
+
+int main ()
+{
+  using namespace my;
+  using namespace my::type_list_meta;
+  static_assert(std::is_same_v<head_t<type_list<int, bool, double>>, int>);
+  static_assert(std::is_same_v<tail_t<type_list<int, bool, double>>, type_list<bool, double>>);
+  static_assert(std::is_same_v<reverse_t<type_list<int, bool, double>>, type_list<double, bool, int>>);
+  static_assert(std::is_same_v<last_t<type_list<int, bool, double>>, double>);
+  static_assert(std::is_same_v<merge_t<type_list<int, bool>, type_list<double, float>, type_list<>>, type_list<int, bool, double, float>>);
+  static_assert(
+    std::is_same_v<
+      typename node_heads<
+        chained_node<
+          or_node<
+            chained_node<inner_node<inner_node<inner_node<>>>, inner_node<inner_node<>>, inner_node<>>,
+            chained_node<inner_node<inner_node<>>, inner_node<>>,
+            inner_node<>
+          >,
+          inner_node<>
+        >
+      >::type,
+      type_list<inner_node<inner_node<inner_node<>>>, inner_node<inner_node<>>, inner_node<>>
+    >
+  );
+  using example_node1 =
+    chained_node<
+      or_node<
+        chained_node<inner_node<inner_node<inner_node<>>>, inner_node<inner_node<>>, inner_node<>>,
+        chained_node<inner_node<inner_node<>>, inner_node<>>,
+        inner_node<>
+      >,
+      inner_node<>
+    >;
+  auto example1 = example_node1{};
+  static_assert(
+    std::is_same_v<
+      decltype(
+        node_heads<example_node1>::get_head_pointers(example1)
+      ),
+      std::tuple<inner_node<inner_node<inner_node<>>>*, inner_node<inner_node<>>*, inner_node<>*>
+    >
+  );
+  return 0;
+}
